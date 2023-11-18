1ca5437e6488981c9d1ef1dd64f041bd008db654
balayokesh <balayokeshmani@gmail.com>
2023-11-18 23:32:18 +0530

    Generated new components, routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DestinationComponent } from './destination/destination.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechnologyComponent } from './technology/technology.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: HomeComponent, title: "Home" },
+    { path: "destination", component: DestinationComponent, title: "Destination" },
+    { path: "crew", component: CrewComponent, title: "Crew" },
+    { path: "technology", component: TechnologyComponent, title: "Technology" },
+];
