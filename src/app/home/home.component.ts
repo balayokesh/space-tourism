db2dba8742161aabeec33d5c1bd69f187ebdc9d4
balayokesh <balayokeshmani@gmail.com>
2023-11-24 00:01:03 +0530

    Completed navbar design for mobile home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,7 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  
 }
